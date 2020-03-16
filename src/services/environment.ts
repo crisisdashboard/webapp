7f061fd4361b622a5835af47e09e52eb668d71b9
Shawn Lee <shawnsmlee@gmail.com>
2020-03-15 17:34:02 -0700

    Update NODE_ENV to be set at build time

--- src/services/environment.ts
+++ src/services/environment.ts
@@ -1,9 +1,10 @@
-import {env} from 'process';
+/** Global constant assigned set on the template at build time. */
+declare const NODE_ENV: string;
 
 /** For accessing environment variables. */
 export class EnvironmentService {
   /** Whether the app is running is production mode. */
   isProduction() {
-    return env.NODE_ENV === 'production';
+    return NODE_ENV === 'production';
   }
 }
