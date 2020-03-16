84d0cf7ebc6c821447b30f1b6f4a014714ceae63
Shawn Lee <shawnsmlee@gmail.com>
2020-03-15 17:45:17 -0700

    Add globals to window rather than global

--- src/services/environment.ts
+++ src/services/environment.ts
@@ -1,10 +1,8 @@
-/** Global constant assigned set on the template at build time. */
-declare const NODE_ENV: string;
-
 /** For accessing environment variables. */
 export class EnvironmentService {
   /** Whether the app is running is production mode. */
   isProduction() {
-    return NODE_ENV === 'production';
+    // This value is set on window in the template, determined at build time.
+    return (window as any).NODE_ENV === 'production';
   }
 }
